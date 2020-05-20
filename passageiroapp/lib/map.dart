5f2755dcf78c9fa023b6588dc5a43f005f44c02d
msg2 <50235029+msg2@users.noreply.github.com>
2020-05-20 02:33:30 +0100

    Client App working with the server

--- passageiroapp/lib/map.dart
+++ passageiroapp/lib/map.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'login.dart';
-import 'marker.dart';
 import 'package:http/http.dart' as http;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -21,14 +20,7 @@ class _MapPageState extends State<MapPage> {
   SharedPreferences sharedPreferences;
   
   BitmapDescriptor _sourceIcon;
-  CustomMarker _markerInfo;
   double _markerToastPosition = -200;
-  CustomMarker _currentPinData = CustomMarker(
-      pinPath: 'assets/icon_bus.png',
-      location: LatLng(0, 0),
-      locationName: '',
-      labelColor: Colors.grey,
-      linhas: List());
 
 
   void _setSourceIcon() async {
@@ -43,10 +35,12 @@ class _MapPageState extends State<MapPage> {
     getMarkers(); //Server testes
  }
 
+  String timeRecord="Clique na linha para obter a previsão";
   GoogleMapController mapController;
   Set<Marker> markers = Set();
   Set<Marker> markersAPI = Set();
-
+  List linhasParagem = List();
+  var _selectedParagemID;
   final LatLng _center = const LatLng(39.733222, -8.821096);
 
   void _onMapCreated(GoogleMapController controller) {
@@ -57,6 +51,7 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       body: Stack(
         children: <Widget>[
           _mapWidget(),
@@ -68,16 +63,17 @@ class _MapPageState extends State<MapPage> {
           child: Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              margin: EdgeInsets.all(20),
+              //margin: EdgeInsets.all(20),
+              margin: EdgeInsets.symmetric(vertical: 25, horizontal:10),
               height: 100,
               decoration: BoxDecoration(
                   color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
                   boxShadow: <BoxShadow>[
                     BoxShadow(
-                      blurRadius: 20,
+                      blurRadius: 50,
                       offset: Offset.zero,
-                      color: Colors.grey.withOpacity(0.5),
+                      color: Colors.grey.withOpacity(0.50),
                     )
                   ]),
               child: Row(
@@ -85,7 +81,7 @@ class _MapPageState extends State<MapPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   _buildLocationInfo(),
-                  _buildMarkerType(),
+                  //_buildMarkerType(),
                 ],
               ),
             ),
@@ -96,38 +92,16 @@ class _MapPageState extends State<MapPage> {
     );
   }
 
-  fillMarkers(){
-    markers.addAll([ 
-    Marker(
-        markerId: MarkerId('value'),
-        position: LatLng(39.733222, -8.821096),
-        infoWindow: InfoWindow(title: "Paragem Biblioteca")),
-    Marker(
-        markerId: MarkerId('value2'),
-        position: LatLng(39.735196, -8.823338),
-        infoWindow: InfoWindow(title: "Paragem Rotunda")),
-    ]);
-  }
 
   getMarkers() async {
-    final String url = 'http://'+ DotEnv().env['IP_ADDRESS']+'/api/markers';
+    final String url = 'http://'+ DotEnv().env['IP_ADDRESS']+'/api/linhasParagens';
     try {      
-      final response = await http.get(url).timeout(const Duration(seconds: 15));
+      final response = await http.get(url).timeout(const Duration(seconds: 7));
       print("status code: " + response.statusCode.toString() );
       if(response.statusCode==200){
         var markers =jsonDecode(response.body);
-        print(markers[1]);
-        print(markers);
         for (var i=0; i<markers.length; i++){
           var auxlinhas = markers[i]['linhas'];
-          String linhas= "";
-
-          /*for(final linha in auxlinhas){
-            print(linha['id_linha']);
-            linhas+="Linha " + linha['id_linha'].toString() + "\n";
-            //print(guardaLinhas);
-          }
-          print(linhas);*/
 
           setState(() {
             markersAPI.add(
@@ -137,27 +111,15 @@ class _MapPageState extends State<MapPage> {
               icon: _sourceIcon,
               onTap: (){
                 setState(() {
-                 // _currentPinData = _markerInfo;
-                  _currentPinData.pinPath= 'assets/icon_bus.png';
-                  _currentPinData.locationName= "My Location";
-                  _currentPinData.location= LatLng(39.733222, -8.821096);
-                  _currentPinData.linhas= auxlinhas;
-
+                  timeRecord="Clique na linha para obter a previsão";
+                  _selectedParagemID = markers[i]['id_paragem'].toString();
+                  linhasParagem= auxlinhas;
                   _markerToastPosition = 0;
                 });
-                
               }),
             );
-           /* _markerInfo = CustomMarker(
-              pinPath: 'assets/icon_bus.png',
-              locationName: "My Location",
-              location: LatLng(39.733222, -8.821096),
-              labelColor: Colors.blue,
-              linhas: auxlinhas);*/
-          }
-          );
+          });
         }
-        
       }
     }catch(e){
       print(e);
@@ -174,6 +136,7 @@ class _MapPageState extends State<MapPage> {
         onTap: (LatLng location) {
               setState(() {
               _markerToastPosition = -200;
+              timeRecord="Clique na linha para obter a previsão";
           });
         },
         markers: Set.from(markersAPI),
@@ -183,7 +146,8 @@ class _MapPageState extends State<MapPage> {
 
   Widget _buildLocationInfo() {
     List<Widget> widgets = [];
-    for(var linha in _currentPinData.linhas){
+    
+    for(var linha in linhasParagem){
       widgets.add(Padding(
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
         child: Row(
@@ -192,10 +156,11 @@ class _MapPageState extends State<MapPage> {
               minWidth: 110,
               height: 30.0,
               child: RaisedButton(
-              onPressed: () => {},
+              onPressed: ()=> getTime(linha['id_linha'].toString(), _selectedParagemID),
               color: Colors.red,
               padding: EdgeInsets.all(2.0),
               shape: StadiumBorder(),
+              
               child: Row(
                 children: [ Text("Linha "+linha['id_linha'].toString())],
               )
@@ -206,23 +171,24 @@ class _MapPageState extends State<MapPage> {
     }
     return Expanded(
       child: Container(
-        margin: EdgeInsets.only(left: 20),
+        margin: EdgeInsets.only(left: 20,right: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
+          
           children: <Widget>[
-            Row(
+            SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 ...widgets
               ]
-            //Text(
-            //  _currentPinData.locationName,
-            //),
-            ),
+            )),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget> [
-               Text("clique para obter a previsão de tempo",),
+               Text(timeRecord),
               ]),
           ],
         ),
@@ -230,10 +196,25 @@ class _MapPageState extends State<MapPage> {
     );
   }
 
-  Widget _buildMarkerType() {
+  Widget _buildMarkerType() { 
     return Padding(
       padding: EdgeInsets.all(15),
-      child: Image.asset(_currentPinData.pinPath,width: 42,height: 50),
+      child: Image.asset('assets/icon_bus.png',width: 42,height: 50),
     );
+  }
+
+  getTime(String linha, String paragemID) async {
+    //final String url = 'http://'+ DotEnv().env['IP_ADDRESS']+'/api/usedid';
+    print("paragemID: " + paragemID);
+    try {      
+     // final response = await http.get(url).timeout(const Duration(seconds: 7));
+     // print("status code: " + response.statusCode.toString() );
+      setState(() {
+        timeRecord = "Tempo até chegar: " + linha + " min.";
+      });
+    }
+    catch(e){
+      print(e);
+    }
   }
 }
